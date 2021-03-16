4c4a83b3145e56e68a67813cdb7d9e6a5a0999bc
Felicia Wootton <felicia.wootton@gmail.com>
2021-03-16 09:39:06 -0500

    Added function to .then block

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -7,40 +7,59 @@ var headerArray = []
 
 // fetch brewery api
 function brewMe (e) {
+
     e.preventDefault();
+
     var userInput = document.getElementById("input").value
     console.log(userInput);
 
     var brewApi = "https://api.openbrewerydb.org/breweries?by_city=" + userInput;
 
     fetch(brewApi)
+
     .then(function(response){
         console.log(response);
         return response.json()
     })
+
     .then(function(data){
         console.log(data);
+        
+        renderBreweryResults(data);
 
-        // for loop to push header elements into array
-        for(var i = 0; i < brewHeader.length; i++) {
-            headerArray.push(brewHeader[i]);
-        }
-
-        // for loop
-        for(var i = 0; i >= data.length[0] && i <= data.length[3]; i++){
-
-            var name = data[i].name
-            //forEach to populate header with name
-            brewHeader.forEach(myFunction)
-
-            function myFunction(item, index, arr) {
-                arr[index] = item.textContent = name
-            }
-
-                
-
-        }
     })
+}
+
+
+function renderBreweryResults (data) { //For loop creates new divs to contain brewery information
+
+    for(i=0; i<data.length-1; i++) {
+
+        //Create new elements
+        var newDiv1 = document.createElement("div");
+        var newDiv2 = document.createElement("div");
+        var newDiv3 = document.createElement("div");
+        var newH4 = document.createElement("h4");
+        var newP = document.createElement("p");
+
+        //Add classes for styling
+        newDiv1.classList.add("card");
+        newDiv2.classList.add("card-content"); 
+        newDiv3.classList.add("content"); 
+    
+        newDiv1.appendChild(newDiv2);
+        newDiv2.appendChild(newDiv3);
+
+        //Insert data from the API call into the new elements
+        newH4.innerHTML = data[i].name;
+        newP.innerHTML = data[i].street + "<br/>" + data[i].city + ", " + data[i].state + "<br/>" + data[i].postal_code + "<br/><br/> Phone: " + data[i].phone;
+    
+        newDiv3.appendChild(newH4);
+        newDiv3.appendChild(newP);
+
+        document.body.appendChild(newDiv1);
+       
+    }
 }
 
 // pull name, street, city, state, postal_code, website_url, phone
